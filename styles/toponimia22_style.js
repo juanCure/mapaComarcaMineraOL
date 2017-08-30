9eda746f74673db72d9c199d47c5eb9bde66e580
lruiz05 <luis05021990@gmail.com>
2017-08-30 15:40:55 -0500

    Refactoring toponimia22_style.js

--- styles/toponimia22_style.js
+++ styles/toponimia22_style.js
@@ -3,7 +3,32 @@ var size = 0;
 var styleCache_toponimia22={}
 var style_toponimia22 = function(feature, resolution){
     console.log("resolucion del mapa: ", map.getView().getResolution())
-    var context = {
+    lyr_toponimia22.setTextPathStyle(function (f)
+    {
+      var text
+      switch(f.get('tipo')){
+        case 'geoforma':
+          text = toponimiaGeoforma(f)
+          break;
+        case 'rio':
+          text = toponimiaRio(f)
+          break;
+        case 'municipio':
+          text = toponimiaMunicipio(f)
+          break;
+        case 'capital':
+          text = toponimiaCapital(f)
+          break;
+        default:
+          text = toponimiaDefault(f)
+      }
+      return [ new ol.style.Style({ text: text, geometry: f.getGeometry().cspline() })]
+    },
+    75);
+
+    //codigo Generado por Qgis2web, pero que no sirve
+
+/*    var context = {
         feature: feature,
         variables: {}
     };
@@ -39,113 +64,100 @@ var style_toponimia22 = function(feature, resolution){
     }
     var allStyles = [styleCache_toponimia22[key]];
     allStyles.push.apply(allStyles, style);
-    return allStyles;
+    return allStyles;*/
 };
 
 var setTextPathStyle = function() {
   lyr_toponimia22.setTextPathStyle(function (f)
   {
+    var text
     switch(f.get('tipo')){
       case 'geoforma':
-        return toponimiaGeoforma(f)
+        text = toponimiaGeoforma(f)
         break;
       case 'rio':
-        return toponimiaRio(f)
+        text = toponimiaRio(f)
         break;
       case 'municipio':
-        return toponimiaMunicipio(f)
+        text = toponimiaMunicipio(f)
         break;
       case 'capital':
-        return toponimiaCapital(f)
+        text = toponimiaCapital(f)
         break;
       default:
-        return toponimiaDefault(f)
+        text = toponimiaDefault(f)
     }
+    return [ new ol.style.Style({ text: text, geometry: f.getGeometry().cspline() })]
   },
   75);
 }
 
 function toponimiaGeoforma(f) {
-  return [ new ol.style.Style(
-    { text: new ol.style.TextPath(
-      { text: f.get("nombre"),
-        font: "13px ubuntu, sans-serif",
-        fill: new ol.style.Fill ({ color:'rgba(0, 0, 0, 1)' }),
-        stroke: new ol.style.Stroke({ color:'rgba(0, 0, 0, 1)', width:0 }),
-        textBaseline: 'middle',
-        textAlign: 'center',
-        rotateWithView: true,
-        textOverflow: 'visible',
-        minWidth: 0
-      }),
-      geometry: f.getGeometry().cspline()
-    })]
+  return new ol.style.TextPath({
+    text: f.get("nombre"),
+    font: "12px ubuntu, sans-serif",
+    fill: new ol.style.Fill ({ color:'rgba(0, 0, 0, 1)' }),
+    stroke: new ol.style.Stroke({ color:'rgba(0, 0, 0, 1)', width:0 }),
+    textBaseline: 'middle',
+    textAlign: 'center',
+    rotateWithView: true,
+    textOverflow: 'visible',
+    minWidth: 0
+  })
 }
 
 function toponimiaRio(f) {
-  return [ new ol.style.Style(
-    { text: new ol.style.TextPath(
-      { text: f.get("nombre"),
-        font: "20px italic, calibri, sans-serif",
-        fill: new ol.style.Fill ({ color:'rgba(0, 0, 255, 1)' }),
-        stroke: new ol.style.Stroke({ color:"#0073e6", width:0 }),
-        textBaseline: 'middle',
-        textAlign: 'center',
-        rotateWithView: true,
-        textOverflow: 'visible',
-        minWidth: 0
-      }),
-      geometry: f.getGeometry().cspline()
-    })]
+  return new ol.style.TextPath({ 
+    text: f.get("nombre"),
+    font: "20px italic, calibri, sans-serif",
+    fill: new ol.style.Fill ({ color:'rgba(0, 0, 255, 1)' }),
+    stroke: new ol.style.Stroke({ color:"#0073e6", width:0 }),
+    textBaseline: 'middle',
+    textAlign: 'center',
+    rotateWithView: true,
+    textOverflow: 'visible',
+    minWidth: 0
+  })
 }
 
 function toponimiaMunicipio(f) {
-  return [ new ol.style.Style(
-    { text: new ol.style.Text(
-      { text: f.get("nombre"),
-        font: "ligth 9px italic, calibri, sans-serif",
-        fill: new ol.style.Fill ({ color:'#000' }),
-        stroke: new ol.style.Stroke({ color:'#000', width:-1 }),
-        textBaseline: 'middle',
-        textAlign: 'center',
-        rotateWithView: true,
-        textOverflow: 'visible',
-        minWidth: 0
-      }),
-      geometry: f.getGeometry().cspline()
-    })]
+  return new ol.style.TextPath({ 
+    text: f.get("nombre"),
+    font: "ligth 9px italic, calibri, sans-serif",
+    fill: new ol.style.Fill ({ color:'#000' }),
+    stroke: new ol.style.Stroke({ color:'#000', width:-1 }),
+    textBaseline: 'middle',
+    textAlign: 'center',
+    rotateWithView: true,
+    textOverflow: 'visible',
+    minWidth: 0
+  })
 }
 
 function toponimiaCapital(f) {
-  return [ new ol.style.Style(
-    { text: new ol.style.TextPath(
-      { text: f.get("nombre"),
-        font: "bold 12px italic, calibri, sans-serif",
-        fill: new ol.style.Fill ({ color:'rgba(0, 0, 0, 1)' }),
-        stroke: new ol.style.Stroke({ color:'rgba(0, 0, 0, 1)', width:0 }),
-        textBaseline: 'middle',
-        textAlign: 'center',
-        rotateWithView: true,
-        textOverflow: 'visible',
-        minWidth: 0
-      }),
-      geometry: f.getGeometry().cspline()
-    })]
+  return new ol.style.TextPath({ 
+    text: f.get("nombre"),
+    font: "bold 12px italic, calibri, sans-serif",
+    fill: new ol.style.Fill ({ color:'rgba(0, 0, 0, 1)' }),
+    stroke: new ol.style.Stroke({ color:'rgba(0, 0, 0, 1)', width:0 }),
+    textBaseline: 'middle',
+    textAlign: 'center',
+    rotateWithView: true,
+    textOverflow: 'visible',
+    minWidth: 0
+  })
 }
 
 function toponimiaDefault(f) {
-  return [ new ol.style.Style(
-    { text: new ol.style.TextPath(
-      { text: f.get("nombre"),
-        font: "15px Arial",
-        fill: new ol.style.Fill ({ color:"#369" }),
-        stroke: new ol.style.Stroke({ color:"#fff", width:3 }),
-        textBaseline: 'middle',
-        textAlign: 'center',
-        rotateWithView: true,
-        textOverflow: 'visible',
-        minWidth: 0
-      }),
-      geometry: f.getGeometry().cspline()
-    })]
+  return new ol.style.TextPath({ 
+    text: f.get("nombre"),
+    font: "15px Arial",
+    fill: new ol.style.Fill ({ color:"#369" }),
+    stroke: new ol.style.Stroke({ color:"#fff", width:3 }),
+    textBaseline: 'middle',
+    textAlign: 'center',
+    rotateWithView: true,
+    textOverflow: 'visible',
+    minWidth: 0
+  })
 }
